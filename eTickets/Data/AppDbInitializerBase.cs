fc16307a16b448d62458ae62e7c1791f11a37176
Fat Halimi <halimifat@gmail.com>
2022-03-02 16:52:17 +0100

    Commit 03-03

---
+++ eTickets/Data/AppDbInitializerBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace eTickets.Data
+{
+    public class AppDbInitializerBase
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder) { }
+    }
+}
--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -2,6 +2,7 @@ using eTickets.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +26,7 @@ namespace eTickets
         public void ConfigureServices(IServiceCollection services)
         {
             //DbContext configuation
-            services.AddDbContext<AppDbContext>();
+            services.AddDbContext<AppDbContext>(options=>options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
             services.AddControllersWithViews();
         }
 
@@ -55,6 +56,8 @@ namespace eTickets
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            //Seed database
+            AppDbInitializer.Seed(app);
         }
     }
 }
