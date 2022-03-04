1f23da75a668bd77719766d2933816b81b332327
Fat Halimi <halimifat@gmail.com>
2022-03-04 16:50:31 +0100

    03-04

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,28 @@
+﻿using eTickets.Data;
+using eTickets.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IActorsService _service;
+        public ActorsController(IActorsService service)
+        {
+            _service = service;
+        }
+        public async  Task<IActionResult> Index()
+        {
+            var data = await _service.GetAll();
+            return View(data);
+        }
+
+        // Get: Actors / create
+        public  IActionResult Create()
+        {
+            return View(); 
+        }
+    }
+}
--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -7,8 +7,11 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name ="Cinema logo")]
         public string Logo { get; set; }
+        [Display (Name ="Cinema name")]
         public string Name { get; set; }
+        [Display (Name="Description")]
         public string Description { get; set; }
 
         //relationships
