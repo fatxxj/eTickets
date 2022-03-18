87b16024c739f9fdddb9de536acc0627fde47b3a
Fat Halimi <halimifat@gmail.com>
2022-03-18 14:57:59 +0100

    Folder 12 Done - Paypal working succesffully

---
+++ eTickets/Models/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; }
+        public int MovieId { get; set; }
+        [ForeignKey("MovieId")]
+        public virtual Movie Movie{ get; set; }
+
+
+        //Relationship with order and item
+        public int OrderId { get; set; }
+        [ForeignKey("OrderId")]
+        public virtual Order Order { get; set; }
+    }
+}
